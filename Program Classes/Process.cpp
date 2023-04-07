6a28c82bcd9aaf8f9e69a2e4a44f244f02222dfc
Galal-00 <galalmohamed2003@gmail.com>
2023-04-07 09:12:39 +0200

    Revert "Revert "Added Process class""
    This reverts commit 02166eb3dcc30c81f86b589e206d8fe28aff8a51.

---
+++ Program Classes/Process.cpp
@@ -0,0 +1,84 @@
+#include "Process.h"
+
+Process::Process(int at, int id, int ct, int ior, int iod){
+	set_PID(id);
+	set_AT(at);
+	set_CT(ct);
+	set_IO_R(ior);
+	set_IO_D(iod);
+	set_state(0);
+}
+
+//setters
+void Process::set_PID(int id) {
+	PID = id;
+}
+void Process::set_AT(int at) {
+	AT = at;
+}
+void Process::set_RT(int rt) {
+	RT = rt;
+}
+void Process::set_CT(int ct) {
+	CT = ct;
+}
+void Process::set_TT(int tt) {
+	TT = tt;
+	set_TRT();
+	set_WT();
+}
+void Process::set_TRT() {
+	TRT = get_TT() - get_AT();
+}
+void Process::set_WT() {
+	WT = get_TRT() - get_CT();
+}
+void Process::set_state(int STT) {
+	state = STT;
+}
+void Process::set_IO_R(int ior) {
+	IO_R = ior;
+}
+void Process::set_IO_D(int iod) {
+	IO_D = iod;
+}
+void Process::set_SIGKILL(bool signal) {
+	SIGKILL = signal;
+}
+
+//getters
+int Process::get_PID() {
+	return PID;
+}
+int Process::get_AT() {
+	return AT;
+}
+int Process::get_RT() {
+	return RT;
+}
+int Process::get_CT() {
+	return CT;
+}
+int Process::get_TT() {
+	return TT;
+}
+int Process::get_TRT() {
+	return TRT;
+}
+int Process::get_WT() {
+	return WT;
+}
+int Process::get_state() {
+	return state;
+}
+int Process::get_IO_R() {
+	return IO_R;
+}
+int Process::get_IO_D() {
+	return IO_D;
+}
+bool Process::get_SIGKILL() {
+	return SIGKILL;
+}
+
+Process::~Process(){}
