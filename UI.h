970c331b48c07376e840f86fbdf5d357641751bb
Galal-00 <galalmohamed2003@gmail.com>
2023-04-11 01:31:31 +0200

    Created UI class

---
+++ UI.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "Program Classes/Scheduler.h"
+class UI
+{
+private:
+	int timestep;
+	Processor** processorList;
+	LinkedQueue BlkList;
+	LinkedQueue TrmLsit;
+public:
+};
+
