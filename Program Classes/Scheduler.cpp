a9723b7b59170df9f3f5ec5f3cdf41f337aad29c
EzzCode <ezz05042003@gmail.com>
2023-05-14 23:45:32 +0300

    Don't steal

--- Program Classes/Scheduler.cpp
+++ Program Classes/Scheduler.cpp
@@ -217,7 +217,7 @@ void Scheduler::Steal()
 {
 	if (timeStep % STL == 0)
 	{
-		while (getSTL_limit() > 0.4)
+		while (getSTL_limit() > 0.4 && (TrmList.GetCount() + BlkList.GetCount()) - noProcesses > 1)
 		{
 			set_SQF_LQF(0);
 			Process* s = LQF->steal();
